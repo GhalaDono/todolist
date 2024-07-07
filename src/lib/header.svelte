bbd52ae6a0c0434c310e527c37a1d96b77813ce9
Ghala <aghala937@gmail.com>
2024-07-07 15:07:19 +0300

    component

---
+++ src/lib/header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import dayjs from 'dayjs';
+    import 'dayjs/locale/ar-sa';
+
+    dayjs.locale('ar-sa');
+    
+    let period = dayjs().format('a') == 'م' ? 'مساء' : 'صباح';
+</script>
+
+<header class="flex justify-between">
+    <div>
+        <h1 class="text-4xl  text-black">
+            {period} الخير 👋
+        </h1>
+        <h2 class="text-2xl text-[#6e6f6f] mt-3">
+            {dayjs().format('اليوم، dddd D MMMM YYYY')}
+        </h2>
+    </div>
+
+    <div class="bg-[#ffffff] h-[3.2rem] w-44 rounded-md">
+
+    </div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,8 @@
 <script lang="ts">
-	import dayjs from 'dayjs';
-	import 'dayjs/locale/ar-sa';
-
-	dayjs.locale('ar-sa');
-	let period=dayjs().format('a')=='م'? 'مساء':'صباح';
+ import Header from "$lib/header.svelte";
 </script>
 
 <div class="p-12">
-	<header class="flex justify-between"> 
-		<div>
-		<h1 class="text-4xl  text-black">
-	   {period} الخير 👋
-		</h1>
-		<h2 class="text-2xl text-[#6e6f6f] mt-3">
-       {dayjs().format('اليوم، dddd D MMMM YYYY')}
-		</h2>
-	</div>
-
-	<div class="bg-[#ffffff] h-[3.2rem] w-44 rounded-md">
-
-	</div>
-	</header>
+	<Header/>
+	<!--اللي سويناه لما فصلنا الاكواد اسمه component-->
 </div>
